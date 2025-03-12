3e9c3d5f31749d2eab9949b9c12789d7b5619917
Parth Rathod <rathodparth02@gmail.com>
2025-03-12 15:31:43 -0500

    support single file execution

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,7 +1,6 @@
 import os
 import pytest
 import tempfile
-from unittest.mock import patch
 
 from magic_eraser.core import eraser
 from magic_eraser.utils.image import load_image
@@ -9,7 +8,7 @@ from magic_eraser.config.config import Config
 from magic_eraser.model_initialization.initialize import ModelInitializer
 from magic_eraser.pipeline import Pipeline
 
-data_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), "sample_data")
+data_root = str(os.path.join(os.path.dirname(os.path.dirname(__file__)), "sample_data"))
 
 
 @pytest.fixture(scope="module")
@@ -40,27 +39,21 @@ def test_entire_code(default_config_erase_humans, mode, num_workers):
 
     with tempfile.TemporaryDirectory() as output_temp_dir:
 
-        with patch("os.path.isdir", autospec=True) as mocked_isdir:
-            mocked_isdir.return_value = True
+        success_cnt, error_cnt = eraser(
+            config_dict=default_config_erase_humans,
+            input_path=os.path.join(data_root, "segmentation", "sample_1.jpg"),
+            output_folder_dir=output_temp_dir,
+            num_workers=num_workers,
+        )
 
-            success_cnt, error_cnt = eraser(
-                config_dict=default_config_erase_humans,
-                input_folder_path=os.path.join(
-                    data_root,
-                    "segmentation",
-                ),
-                output_folder_path=output_temp_dir,
-                num_workers=num_workers,
-            )
+        expected_output_format = ".PNG"
+        assert error_cnt == 0
+        assert success_cnt > 0
 
-            expected_output_format = ".PNG"
-            assert error_cnt == 0
-            assert success_cnt > 0
+        actual_files = [
+            file
+            for file in os.listdir(output_temp_dir)
+            if file.endswith(expected_output_format)
+        ]
 
-            actual_files = [
-                file
-                for file in os.listdir(output_temp_dir)
-                if file.endswith(expected_output_format)
-            ]
-
-            assert len(actual_files) == success_cnt
+        assert len(actual_files) == success_cnt
