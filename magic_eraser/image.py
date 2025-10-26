b99b99f3582c175c9f04443d1754fde46a80ad69
Parth Rathod <rathodparth02@gmail.com>
2025-10-25 20:03:25 -0500

    refactor

--- magic_eraser/image.py
+++ magic_eraser/image.py
@@ -3,89 +3,17 @@ import scipy
 import torchvision.transforms.functional as TVF
 
 from magic_eraser.segmentation.base import SegmentationModel
-from magic_eraser.segmentation.utils.segmentation_output import SegmentationOutput
-from magic_eraser.utils.image import (
-    dilate_boolean_tensors,
-    rgb_to_grayscale,
-)
-from magic_eraser.segmentation.utils.helper import (
-    get_segmentation_masks,
-    filter_mask,
-)
+from magic_eraser.utils.image import rgb_to_grayscale
+
 from magic_eraser.inpainting.base import InpaintingModel
 from magic_eraser.ocr.base import OCRModel
 from magic_eraser.ocr.utils.ocr_output import OCRResult
-
-
-def perform_segmentation(
-    image_tensor: torch.Tensor, segmentation_model: SegmentationModel
-) -> list[SegmentationOutput]:
-    """
-    Performs segmentation on the given image tensor using the provided segmentation model.
-
-    Args:
-        image_tensor (torch.Tensor): The input image tensor to perform segmentation on.
-        segmentation_model (SegmentationModel): An instance of a SegmentationModel subclass.
-
-    Returns:
-        List (SegmentationOutput): A list containing the segmentation results.
-
-    Raises:
-        AssertionError: If the segmentation model is None.
-
-    """
-    assert segmentation_model is not None
-
-    segmentation_output = segmentation_model.inference(image_tensor=image_tensor)
-
-    return segmentation_output
-
-
-def perform_inpainting(
-    image_tensor: torch.Tensor, masks: torch.Tensor, inpainting_model: InpaintingModel
-) -> torch.Tensor:
-    """
-    Performs inpainting on the given image tensor using the provided inpainting model and masks.
-
-    Args:
-        image_tensor (torch.Tensor): The input image tensor to be inpainted.
-        masks (torch.Tensor): A tensor containing boolean masks indicating which regions need inpainting.
-            These masks should have the same shape as the image_tensor but with True values where inpainting is needed.
-        inpainting_model (InpaintingModel): An instance of an InpaintingModel subclass.
-
-    Returns:
-        torch.Tensor: The inpainted image tensor.
-            This output has the same shape as the input image_tensor, but with the masked regions replaced by inpainted content.
-
-    Raises:
-        AssertionError: If the inpainting model is None or not properly configured.
-    """
-    assert inpainting_model is not None
-
-    inpainted_image = inpainting_model.inference(image=image_tensor, mask=masks)
-
-    return inpainted_image
-
-
-def perform_ocr(image_tensor: torch.Tensor, ocr_model: OCRModel) -> list[OCRResult]:
-    """
-    Performs OCR on the given image tensor using the provided ocr model.
-
-    Args:
-        image_tensor (torch.Tensor): The input image tensor to perform ocr on.
-        ocr_model (OCRModel): An instance of a OCRModel subclass.
-
-    Returns:
-        List[OCRResult]: A list of ocr results.
-
-    Raises:
-        AssertionError: If the ocr model is None.
-
-    """
-    assert ocr_model is not None
-
-    ocr_output = ocr_model.inference(image_tensor=image_tensor)
-    return ocr_output
+from magic_eraser.helper import (
+    perform_segmentation,
+    perform_inpainting,
+    perform_ocr,
+    post_process_mask,
+)
 
 
 def erase(
@@ -125,7 +53,7 @@ def erase(
     dilated_segmented_mask = post_process_mask(
         image_tensor,
         segmentation_output,
-        target_labels=target_labels,
+        filter_labels=target_labels,
     )
     if dilated_segmented_mask.any():
         inpainted_image = perform_inpainting(
@@ -162,7 +90,7 @@ def color_splash(
     dilated_segmented_mask = post_process_mask(
         image_tensor,
         segmentation_output,
-        target_labels=target_labels,
+        filter_labels=target_labels,
         dilate_tensors=False,
     )
 
@@ -205,7 +133,7 @@ def remove_background(
     dilated_segmented_mask = post_process_mask(
         image_tensor,
         segmentation_output,
-        target_labels=target_labels,
+        filter_labels=target_labels,
         dilate_tensors=False,
     )
 
@@ -289,7 +217,7 @@ def fall_color(
     dilated_segmented_mask = post_process_mask(
         image_tensor,
         segmentation_output,
-        target_labels=["tree"],
+        filter_labels=["tree"],
         dilate_tensors=False,
     )
 
@@ -305,38 +233,3 @@ def fall_color(
     final_img = non_masked_colored_regions + masked_color_enhance_regions
 
     return final_img
-
-
-def post_process_mask(
-    image_tensor: torch.Tensor,
-    segmentation_output: list[SegmentationOutput],
-    target_labels: list,
-    dilate_tensors: bool = True,
-) -> torch.Tensor:
-    """
-    Post-processes the segmentation output to obtain masks for the target object regions only.
-    This function filters out the target object regions from the segmentation output and dilates the resulting masks.
-    The output tensor is of type boolean with shape as (C, H, W).
-
-    Args:
-        image_tensor (torch.Tensor): Original image tensor.
-        segmentation_output (List(SegmentationOutput)): A list of Segmentation output.
-        target_labels (list): List of target object labels to filter from the segmentation output.
-        dilate_tensors (bool): Whether to dilate the resulting masks. Defaults to True.
-
-    Returns:
-        torch.Tensor: Dilated masks for the target object regions only.
-
-    """
-    segmentation_output_with_targets_only = filter_mask(
-        segmentation_output, target_labels
-    )
-
-    segmentation_masks = get_segmentation_masks(
-        og_image=image_tensor, segmentation_output=segmentation_output_with_targets_only
-    )
-
-    if dilate_tensors:
-        segmentation_masks = dilate_boolean_tensors(segmentation_masks)
-
-    return segmentation_masks
